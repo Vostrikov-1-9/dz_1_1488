bac09980ad0ef3366a3adbaf3771c0779a2f2623
Vostrikov-1-9 <90481123+Vostrikov-1-9@users.noreply.github.com>
2022-04-10 11:22:47 +0300

    Create tests_matrix_default.py

---
+++ tests/tests_matrix_default.py
@@ -0,0 +1,20 @@
+from app.matrix_default import Matrix
+import pytest
+
+
+def test_init():
+    a = Matrix()
+    assert a.spisok == [[0]]
+
+
+def test_input(mocker):
+    mocker.patch('builtins.input', side_effect=['3', '3', '1', '1', '1', '1', '2', '3', '4', '5', '6'])
+    f = Matrix()
+    f.input()
+    assert f.spisok == [[1, 1, 1], [1, 2, 3], [4, 5, 6]]
+
+
+def test_str():
+    z = Matrix([[2, 1, 8], [3, 4, 6]])
+    q = str(z)
+    assert q == '2\t1\t8\n3\t4\t6'
